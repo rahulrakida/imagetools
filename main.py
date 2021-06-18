a93cd2cbba935bd5a1adb2e45c538aa383879c33
rahulrakida <41411810+rahulrakida@users.noreply.github.com>
2021-06-18 21:44:48 +0300

    Added command-line options, plus the ability to sharpen images

--- main.py
+++ main.py
@@ -13,17 +13,29 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-import PIL
+
+from PIL import Image, ImageFilter
 import argparse
 import random
 
-"""
+
 parser = argparse.ArgumentParser(description="command-line tool, adds filters to images")
 parser.add_argument('operation', help='Operation to perform on the image.')
 parser.add_argument('filename', help='Image file path/name')
-parser.parse_args()
-"""
+args = parser.parse_args()
 
-infile = PIL.Image.open('image.png')
-outfile = infile.filter(PIL.ImageFilter.BLUR)
-outfile.show()
+# print(args)
+
+try:
+    infile = Image.open(args.filename)
+except:
+    print("error: invalid image file. maybe this file ain't exist?")
+    quit(1)
+if args.operation == 'blur':
+    outfile = infile.filter(ImageFilter.BLUR)
+elif args.operation == 'sharpen':
+    outfile = infile.filter(ImageFilter.SHARPEN)
+else:
+    print("error: invalid operation (not in: blur, sharpen)")
+    quit(1)
+outfile.save(f'image-{str(random.choice(range(start=1000,stop=10000)))}.png') # This saves the file under a random filename
