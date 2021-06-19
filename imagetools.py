96c8d3e2fef9779d77ecf22174151d744432f69e
rahulrakida <41411810+rahulrakida@users.noreply.github.com>
2021-06-19 18:18:13 +0300

    allow program to be used as module
    separate code into functions, allowing import of apply_filter() function. also rename main.py to imagetools.py, and update instructions in README.md

---
+++ imagetools.py
@@ -0,0 +1,59 @@
+#    imagetools - command-line tool, adds filters to images
+#    Copyright (C) 2021 Rahul Wavare
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from PIL import Image, ImageFilter
+import argparse
+import random
+
+def apply_filter(operation, infile):
+    if operation == 'blur':
+        outfile = infile.filter(ImageFilter.BLUR)
+    elif operation == 'sharpen':
+        outfile = infile.filter(ImageFilter.SHARPEN)
+    elif operation == 'smooth':
+        outfile = infile.filter(ImageFilter.SMOOTH)
+    elif operation == 'smooth+':
+        outfile = infile.filter(ImageFilter.SMOOTH_MORE)
+    else:
+        print("error: invalid operation (not in: blur, sharpen, smooth, smooth+)")
+        quit(1)
+    
+    return outfile
+
+
+def main():
+    # get operation, filename from command line options
+    parser = argparse.ArgumentParser(description="command-line tool, adds filters to images")
+    parser.add_argument('operation', help='Operation to perform on the image.')
+    parser.add_argument('filename', help='Image file path/name')
+    args = parser.parse_args()
+
+    # print(args)
+
+    # open image, close if invalid
+    try:
+        infile = Image.open(args.filename)
+    except:
+        print("error: invalid image file. maybe this file ain't exist?")
+        quit(1)
+    
+    outfile = apply_filter(args.operation, infile)
+
+    # save the new image
+    outfile.save(f'image-{str(random.choice(range(1000,10000)))}.png')
+
+if __name__ == '__main__':
+    main()
