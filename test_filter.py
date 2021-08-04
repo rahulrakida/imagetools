b2eaaedb0994f2ab7761b983d3b198fd8ca848b8
Rahul Wavare <rahulrakida@gmail.com>
2021-08-04 20:11:43 +0300

    add codecov + proper tests

---
+++ test_filter.py
@@ -0,0 +1,11 @@
+import imagetools
+from PIL import Image
+from io import BytesIO
+import requests
+
+def test_filter():
+    image = Image('test_files/image.jpg')
+    assert imagetools.apply_filter('blur', image)
+    assert imagetools.apply_filter('sharpen', image)
+    assert imagetools.apply_filter('smooth', image)
+    assert imagetools.apply_filter('smooth+', image)
