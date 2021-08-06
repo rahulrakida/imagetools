9ea8ab82a774a1557d8b8b171ba0f04dee2423d4
Rahul Wavare <rahulrakida@gmail.com>
2021-08-06 13:45:14 +0300

    update tests to be web-based as well

--- test_filter.py
+++ test_filter.py
@@ -2,6 +2,7 @@ import imagetools
 from PIL import Image
 from io import BytesIO
 import requests
+import random
 
 def test_filter():
     image = Image.open('test_files/image.jpg')
@@ -9,3 +10,8 @@ def test_filter():
     assert imagetools.apply_filter('sharpen', image)
     assert imagetools.apply_filter('smooth', image)
     assert imagetools.apply_filter('smooth+', image)
+    image_download = Image.open(BytesIO(requests.get('https://picsum.photos/{}/{}'.format(random.randint(100, 999), random.randint(100, 999)))))
+    assert imagetools.apply_filter('blur', image_download)
+    assert imagetools.apply_filter('sharpen', image_download)
+    assert imagetools.apply_filter('smooth', image_download)
+    assert imagetools.apply_filter('smooth+', image_download)
