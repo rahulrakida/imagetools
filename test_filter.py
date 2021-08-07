55d7396dc8bf76d74cc32551f24ef5ab3ad713a9
Rahul Wavare <rahulrakida@gmail.com>
2021-08-07 12:51:57 +0300

    test compiling with nuitka

--- test_filter.py
+++ test_filter.py
@@ -10,8 +10,12 @@ def test_filter():
     assert imagetools.apply_filter('sharpen', image)
     assert imagetools.apply_filter('smooth', image)
     assert imagetools.apply_filter('smooth+', image)
-    image_download = Image.open(BytesIO(requests.get('https://picsum.photos/{}/{}'.format(random.randint(100, 999), random.randint(100, 999)))))
+    content_bytes = requests.get('https://picsum.photos/{}/{}'.format(random.randint(100, 999), random.randint(100, 999)).content)
+    image_download = Image.open(BytesIO(content_bytes))
     assert imagetools.apply_filter('blur', image_download)
     assert imagetools.apply_filter('sharpen', image_download)
     assert imagetools.apply_filter('smooth', image_download)
     assert imagetools.apply_filter('smooth+', image_download)
+
+if __name__ == "__main__":
+    test_filter()
